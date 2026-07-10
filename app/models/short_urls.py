580630977d378f1c763cdbf5357e18dea00d85db
rwaenrd <963852zxc123@gmail.com>
2026-07-10 13:42:19 +0300

    add default values to column

--- app/models/short_urls.py
+++ app/models/short_urls.py
@@ -9,4 +9,4 @@ class ShortURL(Base):
 
     url: Mapped[str] = mapped_column(String(2048))
     short_code: Mapped[str] = mapped_column(String(8), unique=True)
-    access_count: Mapped[int] = mapped_column(default=0)
+    access_count: Mapped[int] = mapped_column(default=0, server_default="0")
